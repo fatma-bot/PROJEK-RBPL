7003342528d1a63e62ebe091dc54299f3346ae18
Fatma Triana <fatmatriana9@gmail.gom>
2025-06-17 10:39:32 +0700

    pengiriman

--- Pelanggan/update_pengiriman.php
+++ Pelanggan/update_pengiriman.php
@@ -39,13 +39,48 @@ if(ISSET($_POST['submit'])) {
         $query1 = mysqli_query($connect, $sql1);
     }
     else {
-        $sql2 = "INSERT INTO pengantaran (idAntar, idPesanan, statusAntar) VALUES ('', '$idPesanan', 'diproses')";
-        $query2 = mysqli_query($connect, $sql2);
+        $sqlKurir = "SELECT idKurir FROM kurir ORDER BY idKurir ASC"; // Urutkan untuk konsistensi
+        $queryKurir = mysqli_query($connect, $sqlKurir);
+        $kurirIds = [];
+        while ($row = mysqli_fetch_assoc($queryKurir)) {
+            $kurirIds[] = $row['idKurir'];
+        }
+        if (!empty($kurirIds)) {
+            $lastAssignedKurirId = 0;
+            $sqlLastKurir = "SELECT idKurir FROM penjemputan ORDER BY idJemput DESC LIMIT 1";
+            $resultLastKurir = mysqli_query($connect, $sqlLastKurir);
+            if (mysqli_num_rows($resultLastKurir) > 0) {
+                $rowLastKurir = mysqli_fetch_assoc($resultLastKurir);
+                $lastAssignedKurirId = $rowLastKurir['idKurir'];
+            }
+            
+            $currentIndex = array_search($lastAssignedKurirId, $kurirIds);
+            
+            $nextIndex = ($currentIndex !== false && $currentIndex + 1 < count($kurirIds)) ? $currentIndex + 1 : 0;
+            $idKurirYangDitugaskan = $kurirIds[$nextIndex];
+
+            $date = date('Y-m-d');
+
+            $sql3 = "INSERT INTO penjemputan (idJemput, idPesanan, idKurir, tglJemput, statusJemput) VALUES ('', '$idPesanan', '$idKurirYangDitugaskan', '$date', 'Diproses')";
+            $query3 = mysqli_query($connect, $sql3);
+            
+            $sql2 = "INSERT INTO pengantaran (idAntar, idPesanan, idKurir, statusAntar) VALUES ('', '$idPesanan', '$idKurirYangDitugaskan', 'Diproses')";
+            $query2 = mysqli_query($connect, $sql2);
+
+            if (!$query3 || !$query2) {
+                error_log("Error inserting into penjemputan or pengantaran: " . mysqli_error($connect));
+            }
+
+        } else {
+            error_log("No couriers found in the database.");
+        }
+    }
+
     }
     $sql4 = "INSERT INTO pembayaran (idPembayaran, idPesanan, totalBayar, statusPembayaran) VALUES ('', '$idPesanan', '$totalBayar', '$statusPembayaran')";
     $query4 = mysqli_query($connect, $sql4);
     if($query) {
         header('location:pesananDibuat.php?id='.$idPesanan);
     }
-}
+
 ?>
